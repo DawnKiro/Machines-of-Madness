5dfb46f4a4ed380c4f61ff4a7ee3280a7fe27402
DawnKiro <kirobyte4@gmail.com>
2024-12-12 01:34:31 -0600

    Funny log messages, eepy time

--- src/main/java/net/voxelden/machinesOfMadness/MachinesOfMadness.java
+++ src/main/java/net/voxelden/machinesOfMadness/MachinesOfMadness.java
@@ -10,9 +10,17 @@ public class MachinesOfMadness implements ModInitializer {
     public static final String MOD_ID = "machines_of_madness";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    public static final Random RANDOM = new Random();
+    public static final String[] INIT_LOGS = {
+            "Excess is not enough",
+            "It's never enough POWAHHHHHH",
+            "I bet I can get it over 9000",
+            "A MASSIVE ERROR HAS OCCURRED\n\t\tjust kidding ;3"
+    };
+
     @Override
     public void onInitialize() {
-        LOGGER.info("merp");
+        LOGGER.info(INIT_LOGS[RANDOM.nextInt(INIT_LOGS.length - 1)]);
 
         Blocks.register();
     }
